7d4c42630bfc393d69dd95d4a3abb8707e7b0b69
J. Santos <jamillo@gmail.com>
2019-10-28 21:25:49 -0300

    :sparkles: Implement MethodDescriptor.Compatible
    This method enables to compare two MethodDescriptor and check if they are
    compatible. Method are considered compatatibles if they have the same
    signature (only types of arguments and results are considered).

--- method.go
+++ method.go
@@ -18,7 +18,7 @@ type MethodDescriptor struct {
 
 type MethodResult struct {
 	Name string
-	Type Type
+	Type RefType
 }
 
 // NewMethodDescriptor return the pointer of new MethodDescriptor
@@ -36,4 +36,30 @@ func (method *MethodDescriptor) Package() *Package {
 // Name return name of Method
 func (method *MethodDescriptor) Name() string {
 	return method.name
+}
+
+// Compatible checks if the signature of both method descriptor are compatible.
+//
+// It checks if the all arguments refer to the same RefType. The same happens
+// with the result.
+//
+// Receivers are not taken into consideration, neither names.
+func (method *MethodDescriptor) Compatible(m *MethodDescriptor) bool {
+	if len(method.Arguments) != len(m.Arguments) {
+		return false
+	}
+	if len(method.Result) != len(m.Result) {
+		return false
+	}
+	for i, arg := range method.Arguments {
+		if m.Arguments[i].Type != arg.Type {
+			return false
+		}
+	}
+	for i, r := range method.Result {
+		if m.Result[i].Type != r.Type {
+			return false
+		}
+	}
+	return true
 }
